446517b326f62fcb04e668a8cef6eec8a0918ba0
stephanies709 <noe21002@byui.edu>
2022-05-19 14:15:24 -0400

    dom manipulation completed

---
+++ lesson5/dom_manipulation/dom.js
@@ -0,0 +1,23 @@
+const list = document.querySelector('.list');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+button.onclick = function() {
+    let myItem = input.value;
+    input.value = '';
+
+    const listItem = document.createElement('li');
+    const listText = document.createElement('span');
+    const listBtn = document.createElement('button');
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = '❌';
+    list.appendChild(listItem);
+
+    listBtn.onclick = function(e) {
+        list.removeChild(listItem);
+        }
+        input.focus();
+    }
